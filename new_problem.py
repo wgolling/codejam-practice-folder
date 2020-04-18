231638d066edffc2b29ac04d4b4b1e7d1c5f1c67
William Gollinger <wgolling@gmail.com>
2020-04-17 20:56:21 -0400

    code clean-up

--- new_problem.py
+++ new_problem.py
@@ -6,28 +6,29 @@ dir_path = Path(__file__).parent.resolve()
 
 def main(argv):
 
+  # Declare variables.
   year, round_name, name = None, None, None
-  interactive = False
+  interactive = False                                                         # In 2018 interactive problems were added.
 
   # Process arguments.
   try:
-    opts, args = getopt.getopt(argv, "hiy:r:n:", ["year=", "round=", "name="])
+    opts, args = getopt.getopt(argv, 'hiy:r:n:', ['interactive', 'year=', 'round=', 'name='])
   except getopt.GetoptError:
     print_usage()
   else:
     for opt, arg in opts:
       if opt == '-h':
         print_usage(err=False)
-      elif opt == '-i':
+      elif opt in ('-i', '--interactive'):
         interactive = True
-      elif opt in ('-y', "--year"):
+      elif opt in ('-y', '--year'):
         try:
           year = int(arg)
         except:
-          exit("Year must be an integer.")
-      elif opt in ('-r', "--round"):
+          exit('Year must be an integer.')
+      elif opt in ('-r', '--round'):
         round_name = arg
-      elif opt in ('-n', "--name"):
+      elif opt in ('-n', '--name'):
         name = arg        
 
   # Check that required arguments were included.
@@ -38,13 +39,13 @@ def main(argv):
   try:
     p = make_folder(str(year), round_name, name)
   except FileExistsError:
-    exit("Problem folder already exists.")
+    exit('Problem folder already exists.')
 
   # Copy the appropriate template.
-  prefix = copy_template(year, p, interactive=interactive)
-  if len(prefix) > 0:
-    prefix += " "
+  copy_template(year, p, interactive=interactive)
 
+  # Close program.
+  sys.exit(0)
 
 def print_usage(err=True):
   exit('usage: new_problem.py [-h] -y <year> -r <round> -n <name>', err=err)
@@ -63,27 +64,28 @@ def make_folder(year, round_name, name):
   return problem_path
 
 def copy_template(year, path, interactive=False):
-  prefix = ""
-  template_name = prefix
-  # In 2018 two changes were made in the contest format:
-  # 1) Rather than outputting results to a file, the code itself is uploaded
-  #    and outputs to stdout.
-  # 2) There are not Interactive problems.
+  '''
+  In 2018 two changes were made in the contest format:
+  1) Rather than outputting results to a file, the code itself is uploaded
+     and outputs to stdout.
+  2) There are now Interactive problems.
+  '''
+  template_name = ''
+  prefix = ''
   if year < 2018:
     prefix = 'pre2018'
-    template_name = prefix + '-'
   elif interactive:
     prefix = 'interactive'
+  if len(prefix) > 0:
     template_name = prefix + '-'
   template_name += "template.py"
+  # Copy template file.
   template_path = dir_path / 'templates' / template_name
-  dest_path = path / "main.py"
+  dest_path = path / 'main.py'
   shutil.copy(str(template_path), str(dest_path))
-  (path / "tests.in").touch()
+  # Create tests file.
+  (path / 'tests.in').touch()
   print('Copied {} template to {}.'.format(prefix, str(dest_path)))
-  return prefix
-
-
 
 
 if __name__ == "__main__":
